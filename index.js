d6d77e0dbf6845977254f4e810d21015e30df2d7
rtetelbaum <rtetelbaum@mailbox.org>
2020-11-16 10:23:53 -0500

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+	const array = []
+	for (let i = 0; i < names.length; i++) {
+		array.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+	}
+	return array
+}
+
+function countDown(int) {
+	while (int >= 0) {
+		console.log(int)
+		int--
+	}
+}
